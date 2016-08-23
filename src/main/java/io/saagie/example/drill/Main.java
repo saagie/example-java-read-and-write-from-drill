cf89f92f9f7beddcf3fede7403ab5f380ab04bad
Pierre Leresteux <pierre.leresteux@gmail.com>
2016-08-23 20:33:15 +0200

    refacto(log & variable) : Correct usage for logger and for variables

--- src/main/java/io/saagie/example/drill/Main.java
+++ src/main/java/io/saagie/example/drill/Main.java
@@ -3,8 +3,6 @@ package io.saagie.example.drill;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Logger;
 
@@ -24,25 +22,23 @@ public class Main {
 		}
 		// Get url Connection
 		connectionUrl = args[0];
-		System.out.println(connectionUrl);
+		logger.info("connectionUrl : " + connectionUrl);
 		// Init Connection
-		Connection con = null;
+		Connection connection = null;
 
 		//==== Select data from Drill Table
 		String sqlStatement = "SELECT * FROM cp.`employee.json`";
 		try {
 			Class.forName(JDBC_DRIVER_NAME);
-			Connection connection = DriverManager.getConnection(connectionUrl);
+			connection = DriverManager.getConnection(connectionUrl);
 			Statement st = connection.createStatement();
-			ResultSet rs = st.executeQuery(sqlStatement);
+			st.executeQuery(sqlStatement);
 
-		} catch (SQLException e) {
-			e.printStackTrace();
 		} catch (Exception e) {
-			e.printStackTrace();
+			logger.severe(e.getMessage());
 		} finally {
 			try {
-				con.close();
+				connection.close();
 			} catch (Exception e) {
 				// swallow
 			}
